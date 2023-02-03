77dc04ce0a35f9ff9026ef0d64a49203d508017c
Willyan N Pereira <willy4nnp@gmail.com>
2023-02-02 21:10:45 -0300

    tabnav transformado em classe

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,20 +1,29 @@
-export default function initTabNav() {
-  const ativoClass = 'ativo';
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-  function activeTab(index) {
-    tabContent.forEach((section) => section.classList.remove(ativoClass));
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add(ativoClass, direcao);
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add(ativoClass);
+  // ativa a tab de acordo com o index da mesma
 
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
+  activeTab(index) {
+    this.tabContent.forEach((section) => section.classList.remove(this.activeClass));
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.activeClass, direcao);
+  }
+  // Adiciona os eventos nas tabs
+
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index));
     });
   }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      // ativar primeiro item
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
+  }
 }
-initTabNav();
